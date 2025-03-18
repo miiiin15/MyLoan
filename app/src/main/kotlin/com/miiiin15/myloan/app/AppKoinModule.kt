1a301495b07fd74cfa9a3633fb9196bc71ad90e2
mintae <gg04253@gmail.com>
2025-03-18 17:09:33 +0900

    feat: Application 클래스 추가 및 의존성, 네비게이션 연결

---
+++ app/src/main/kotlin/com/miiiin15/myloan/app/AppKoinModule.kt
@@ -0,0 +1,9 @@
+package com.miiiin15.myloan.app
+
+import org.koin.dsl.module
+
+val appModule = module {
+    // TODO : 파이어베이스 의존성 주입
+//    single { FirebaseAuth.getInstance() }
+//    single { FirebaseFirestore.getInstance() }
+}
---
+++ app/src/main/kotlin/com/miiiin15/myloan/app/MyLoanApplication.kt
@@ -0,0 +1,48 @@
+package com.miiiin15.myloan.app
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+import com.miiiin15.myloan.BuildConfig
+import com.miiiin15.myloan.base.baseModule
+import com.miiiin15.myloan.list.featureListModules
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
+import timber.log.Timber
+
+class MyLoanApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKoin()
+        initTimber()
+        initDynamicColorScheme()
+    }
+
+    private fun initDynamicColorScheme() {
+        // Apply dynamic colors to all Activities, Fragments, Views
+        // (Material 3 library helper class)
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+
+    private fun initKoin() {
+        GlobalContext.startKoin {
+            androidLogger()
+            androidContext(this@MyLoanApplication)
+
+            modules(appModule)
+            modules(baseModule)
+            // TODO : feature 모듈 주입
+              modules(featureListModules)
+
+        }
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
