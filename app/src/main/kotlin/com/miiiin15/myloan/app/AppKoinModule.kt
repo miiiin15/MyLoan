2a0118b903f51564a84661e89b44a06938ed4816
mintae <gg04253@gmail.com>
2025-03-20 14:10:22 +0900

    feat: AppKoinModule FirebaseDatabase 의존성 주입

--- app/src/main/kotlin/com/miiiin15/myloan/app/AppKoinModule.kt
+++ app/src/main/kotlin/com/miiiin15/myloan/app/AppKoinModule.kt
@@ -1,9 +1,10 @@
 package com.miiiin15.myloan.app
 
 import org.koin.dsl.module
+import com.google.firebase.database.FirebaseDatabase
 
 val appModule = module {
     // TODO : 파이어베이스 의존성 주입
-//    single { FirebaseAuth.getInstance() }
 //    single { FirebaseFirestore.getInstance() }
+    single { FirebaseDatabase.getInstance() }
 }
