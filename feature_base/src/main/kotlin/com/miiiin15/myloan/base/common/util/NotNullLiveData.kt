01571c57c8911695b3d2d2370e69227f9e69a6ca
mintae <gg04253@gmail.com>
2025-03-25 10:59:22 +0900

    feat: NotNullLiveData 관련 익스텐션 함수 추가

---
+++ feature_base/src/main/kotlin/com/miiiin15/myloan/base/common/util/NotNullLiveData.kt
@@ -0,0 +1,34 @@
+package com.miiiin15.myloan.base.common.util
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asFlow
+import kotlinx.coroutines.flow.Flow
+
+open class NotNullLiveData<T : Any>(value: T) : LiveData<T>(value) {
+    override fun getValue(): T = super.getValue()!!
+
+    @Suppress("RedundantOverride")
+    override fun setValue(value: T) = super.setValue(value)
+
+    @Suppress("RedundantOverride")
+    override fun postValue(value: T) = super.postValue(value)
+}
+
+class NotNullMutableLiveData<T : Any>(value: T) : NotNullLiveData<T>(value) {
+    public override fun setValue(value: T) = super.setValue(value)
+
+    public override fun postValue(value: T) = super.postValue(value)
+}
+
+inline fun <T : Any> NotNullLiveData<T>.observe(
+    owner: LifecycleOwner,
+    crossinline observer: (T) -> Unit,
+) = Observer { value: T -> observer(value) }.also { observe(owner, it) }
+
+
+
+inline fun <T : Any> NotNullLiveData<T>.toFlow(): Flow<T> {
+    return this.asFlow()
+}
